e48e99bd44309e48f1eca85247829c02d0d510d5
zuoming <894328677@qq.com>
2022-03-11 00:16:50 +0800

    1

--- js/public.js
+++ js/public.js
@@ -1,9 +1,12 @@
 // keyControl
 var keyControl = {
     curItem: undefined,
-    curItemGroup: undefined,
-
+    itemIndex: 0,
     lastItem: undefined,
+
+    curItemGroup: undefined,
+    groups: $(".itemGroup"),
+    groupIndex: 0,
 
     keyLeft: function() {
         this.setCurItem(this.curItem.prevAll(".item").eq(0))
@@ -12,7 +15,13 @@ var keyControl = {
         this.setCurItem(this.curItem.nextAll(".item").eq(0))
     },
     keyUp: function() {},
-    keyDown: function() {},
+    keyDown: function() {
+        var targetSize = this.groupIndex < this.groups.length ? this.groupIndex + 1 : this.groupIndex;
+        var originIndex = this.itemIndex;
+        var targetIndex = this.skipGroup(this.curItemGroup.length, targetSize, originIndex);
+        console.log(targetIndex);
+        this.setCurItem(this)
+    },
     keyEnter: function() {},
     keyBack: function() {},
 
@@ -31,13 +40,43 @@ var keyControl = {
 
         this.curItem.trigger("changeFocus")
         this.setCurItemGroup()
+
+        this.itemIndex = this.curItemGroup.find(".item").index(this.curItem);
+        console.log(this.itemIndex);
     },
     setCurItemGroup(target) {
         this.curItemGroup = target || this.curItem.parents(".itemGroup")
     },
-}
+    setGroups() {
+        this.groups = $(".itemGroup");
+    },
+    skipGroup(originSize, targetSize, originIndex) {
+        console.log(originSize, targetSize, originIndex);
+        let targetIndex = 0;
+        let originSizeMid = originSize / 2;
 
-keyControl.setCurItem($("#menu .item").eq(0));
+        targetIndex = (targetSize / originSize) * originIndex;
+        if (originSizeMid <= originIndex) {
+            targetIndex = Math.ceil(targetIndex);
+        } else {
+            targetIndex = Math.floor(targetIndex);
+        }
+
+        if (targetIndex < 0) {
+            targetIndex = 0;
+        } else if (targetIndex > targetSize - 1) {
+            targetIndex = targetSize - 1;
+        }
+
+        return targetIndex;
+    },
+    init() {
+        this.setGroups();
+        if (!this.curItem) {
+            this.setCurItem(this.groups.eq(0).find(".item").eq(0));
+        }
+    },
+}
 
 $(window).on("keyup", function(e) {
     switch (e.keyCode) {
@@ -65,7 +104,8 @@ $(window).on("keyup", function(e) {
 })
 
 $("#recommend").load("/snippet/recommend1.html", function() {
-
+    keyControl.init()
+    console.log(keyControl);
 });
 
 // 绑定事件
